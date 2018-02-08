482674e6d244f0eb2fd80cfdbe8f8358d6427973
andrewinnocent <innocent.andrew@gmail.com>
2018-02-07 23:08:58 -0500

    Created Game Logic, HTML, CSS Files
    Pseudo code and initial gameBoard created, added bootstrap grid to HTML,
    and basic styling to grid.

---
+++ assets/scripts/game-logic.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Files to be required(if any):
+
+// Create game board
+
+// Array where each index is where a user can position an X or O
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+// Another look at the board is:
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+// When a player clicks on a specific block (represented by the number/index),
+// the return will be an X or O.
+
+module.export = {
+  gameBoard
+}

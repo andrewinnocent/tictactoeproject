4716006ca714749b192d1dc8f3a4d9d873c13447
andrewinnocent <innocent.andrew@gmail.com>
2018-02-08 11:31:34 -0500

    Click addHandler Added
    Each box in the grid has a click handler and a console log representing
    which box was clicked.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const clickEvents = require('./events')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +13,7 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$(() => {
+  clickEvents.addHandlers()
+})

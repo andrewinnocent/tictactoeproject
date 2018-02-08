12017929df3cfc264fd0822e45fe19b53278a38f
andrewinnocent <innocent.andrew@gmail.com>
2018-02-08 09:39:15 -0500

    Initial Function for Click
    Nothing really done here.

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -7,13 +7,17 @@
 // Array where each index is where a user can position an X or O
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
-// Another look at the board is:
+// Another look at the board is index:
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
 // When a player clicks on a specific block (represented by the number/index),
-// the return will be an X or O.
+// the return will be an X or O. Always start with X.
+
+const play = function () {
+
+}
 
 module.export = {
   gameBoard

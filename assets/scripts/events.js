2077816c4ea54e4c7f88018a0caeca9c79e112ba
andrewinnocent <innocent.andrew@gmail.com>
2018-02-13 07:27:25 -0500

    Reset button
    Added functioning reset button to clear board.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,7 +4,7 @@ const api = require('./api')
 const getFormFields = require('../../lib/get-form-fields')
 const ui = require('./ui')
 
-// Make messages disappear after 3-5 secs
+// Make messages disappear after 3-5 secs (desired)
 const onSignUp = function (event) {
   event.preventDefault()
   $('#sign-up').modal('hide')
@@ -26,51 +26,22 @@ const onSignIn = function (event) {
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
 }
+
+const addHandlers = () => {
+  $('#onSignUp').on('submit', onSignUp)
+  $('#onSignIn').on('submit', onSignIn)
+}
+
 // When signed in:
 // Remove sign-up button, change log in to log out, add change password and scores
 // buttons.
 
 // Add "Play" button to start a game with a clear gameBoard.
 
-// Add a "Reset" button to start a game over.
-
-// addHandlers for grid spaces clicked on. Also turns are determined and placed in
-// the grid space selected!
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
+// Player turns [WORKS]
 let turn = 0
-// const boxSelect = () => {
-//   $('.col-xs-4').click(function () { // applies the click method to a specific grid space (div)
-//     if ((turn % 2 === 0) && ($(this.id).text() === true)) { // even turns are X, odd turns are O
-//       $(this).text('X') // "this" is the grid space clicked on and X is added to it
-//       gameBoard[this.id] = 'X' // X is placed in the specific index that matched the grid space clicked on
-//     } else if ((turn % 2 === 1) && ($(this.id).text() === true)) {
-//       $(this).text('O')
-//       gameBoard[this.id] = 'O'
-//     } else {
-//       document.getElementById(this.id).disabled = true
-//     }
-//     console.log(gameBoard)
-//     checkForWin()
-//     turn++ // adds 1 to the turn being played
-//   })
-// }
-
-// const boxSelect = () => {
-//   $('.col-xs-4').click(function () { // applies the click method to a specific grid space (div)
-//     if (turn % 2 === 0) { // even turns are X, odd turns are O
-//       $(this).text('X') // "this" is the grid space clicked on and X is added to it
-//       gameBoard[this.id] = 'X' // X is placed in the specific index that matched the grid space clicked on
-//     } else {
-//       $(this).text('O')
-//       gameBoard[this.id] = 'O'
-//     }
-//     console.log(gameBoard)
-//     checkForWin()
-//     turn++ // adds 1 to the turn being played
-//   })
-// }
-
 const boxSelect = () => {
   for (let i = 0; i < gameBoard.length; i++) {
     $('#' + i).click(function () { // applies the click method to a specific grid space (div)
@@ -78,7 +49,7 @@ const boxSelect = () => {
         $(this).text('X') // "this" is the grid space clicked on and X is added to it
         gameBoard[this.id] = 'X' // X is placed in the specific index that matched the grid space clicked on
         console.log(this)
-        $(this).attr('disabled', true)
+        $(this).attr('disabled', true) // disables button once selected
       } else {
         $(this).text('O')
         gameBoard[this.id] = 'O'
@@ -91,7 +62,7 @@ const boxSelect = () => {
   }
 }
 
-// checks for wins
+// checks for wins & locks board when game done [WORKS]
 const checkForWin = function () {
   if (// 0, 1, 2
     (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] === 'X') ||
@@ -110,7 +81,7 @@ const checkForWin = function () {
     // 6, 7, 8
     (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] === 'X')) {
     console.log('X won!')
-    $('.table-button').attr('disabled', true)
+    $('.table-button').attr('disabled', true) // disables board if X wins
     $('#game-message').text('X wins!')
     $('#game-message').css('background-color', 'blue')
   } else if (// 0, 1, 2
@@ -130,7 +101,7 @@ const checkForWin = function () {
     // 6, 7, 8
     (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] === 'O')) {
     console.log('O won!')
-    $('.table-button').attr('disabled', true)
+    $('.table-button').attr('disabled', true) // disables board if O wins
     $('#game-message').text('O wins!')
     $('#game-message').css('background-color', 'blue')
   } else if (gameBoard.includes('') === false) {
@@ -142,13 +113,22 @@ const checkForWin = function () {
   }
 }
 
-// How to block a space selected??
+// Game restart [WORKS]
+$('#restart').on('click', function () {
+  for (let i = 0; i < gameBoard.length; i++) {
+    $('#' + i).text('') // clears X or O in each button
+    $('.table-button').attr('disabled', false) // allows buttons to be clicked on again
+    gameBoard[i] = '' // clears array of X & O, back to empty string
+    turn = 0 // resets turn to start with X
+  }
+  console.log(gameBoard)
+  console.log('reset!')
+})
 
-const addHandlers = () => {
-  $('#onSignUp').on('submit', onSignUp)
-  $('#onSignIn').on('submit', onSignIn)
-}
-
+// Store wins/losses
+const wins = []
+const losses = []
+let winTally = 0
 module.exports = {
   boxSelect,
   addHandlers
